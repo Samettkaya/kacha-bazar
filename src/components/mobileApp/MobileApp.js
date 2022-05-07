27599622f1e665b207504f728713e8a01f5a02e7
samettkaya <kaya67380@gmail.com>
2022-05-07 14:01:17 +0300

    mobile app code refactoring

--- src/components/mobileApp/MobileApp.js
+++ src/components/mobileApp/MobileApp.js
@@ -25,158 +25,90 @@ function MobileApp() {
                 maxWidth: "100%",
               }}
             >
-              <img
-                className="block w-auto"
-                // style={{
-                //   position: "absolute",
-                //   top: "0",
-                //   left: "0",
-                //   bottom: "0",
-                //   right: "0",
-                //   boxSizing: "border-box",
-                //   padding: "0",
-                //   border: "none",
-                //   margin: "auto",
-                //   display: "block",
-                //   width: "0",
-                //   height: "0",
-                //   minwidth: "100%",
-                //   maxWidth: "100%",
-                //   minHeight: "100%",
-                //   maxHeight: "100%",
-                // }}
-                src={AppDownloadLeft}
-              />
+              <img className="block w-auto" src={AppDownloadLeft} />
             </span>
           </div>
           <div className="text-center">
-          <h3 className="text-xl md:text-2xl lg:text-3xl font-bold  mb-3">Get Your Daily Needs From Our KachaBazar Store</h3>
-          <p className="text-base opacity-90 leading-7">There are many products you will find our shop, Choose your daily necessary product from our KachaBazar shop and get some special offer.</p>
-          <div className="mt-8 block lg:flex">
-              <a className="mx-2" target="_blank" href="https://www.apple.com/app-store/">
-              <span
-              style={{
-                boxSizing: "border-box",
-                display: "inline-block",
-                overflow: "hidden",
-                width: "initial",
-                height: "initial",
-                background: "none",
-                opacity: "1",
-                border: "0",
-                margin: "0",
-                padding: "0",
-                position: "relative",
-                maxWidth: "100%",
-              }}
-            >
-              <img
-                className="block w-auto"
-                // style={{
-                //   position: "absolute",
-                //   top: "0",
-                //   left: "0",
-                //   bottom: "0",
-                //   right: "0",
-                //   boxSizing: "border-box",
-                //   padding: "0",
-                //   border: "none",
-                //   margin: "auto",
-                //   display: "block",
-                //   width: "0",
-                //   height: "0",
-                //   minwidth: "100%",
-                //   maxWidth: "100%",
-                //   minHeight: "100%",
-                //   maxHeight: "100%",
-                // }}
-                src={AppStore}
-              />
-            </span>
+            <h3 className="text-xl md:text-2xl lg:text-3xl font-bold  mb-3">
+              Get Your Daily Needs From Our KachaBazar Store
+            </h3>
+            <p className="text-base opacity-90 leading-7">
+              There are many products you will find our shop, Choose your daily
+              necessary product from our KachaBazar shop and get some special
+              offer.
+            </p>
+            <div className="mt-8 block lg:flex">
+              <a
+                className="mx-2"
+                target="_blank"
+                href="https://www.apple.com/app-store/"
+              >
+                <span
+                  style={{
+                    boxSizing: "border-box",
+                    display: "inline-block",
+                    overflow: "hidden",
+                    width: "initial",
+                    height: "initial",
+                    background: "none",
+                    opacity: "1",
+                    border: "0",
+                    margin: "0",
+                    padding: "0",
+                    position: "relative",
+                    maxWidth: "100%",
+                  }}
+                >
+                  <img className="block w-auto" src={AppStore} />
+                </span>
               </a>
-              <a className="mx-2" target="_blank" href="https://play.google.com/store/apps">
-              <span
-              style={{
-                boxSizing: "border-box",
-                display: "inline-block",
-                overflow: "hidden",
-                width: "initial",
-                height: "initial",
-                background: "none",
-                opacity: "1",
-                border: "0",
-                margin: "0",
-                padding: "0",
-                position: "relative",
-                maxWidth: "100%",
-              }}
-            >
-              <img
-                className="block w-auto"
-                // style={{
-                //   position: "absolute",
-                //   top: "0",
-                //   left: "0",
-                //   bottom: "0",
-                //   right: "0",
-                //   boxSizing: "border-box",
-                //   padding: "0",
-                //   border: "none",
-                //   margin: "auto",
-                //   display: "block",
-                //   width: "0",
-                //   height: "0",
-                //   minwidth: "100%",
-                //   maxWidth: "100%",
-                //   minHeight: "100%",
-                //   maxHeight: "100%",
-                // }}
-                src={PlayStore}
-              />
-            </span>
+              <a
+                className="mx-2"
+                target="_blank"
+                href="https://play.google.com/store/apps"
+              >
+                <span
+                  style={{
+                    boxSizing: "border-box",
+                    display: "inline-block",
+                    overflow: "hidden",
+                    width: "initial",
+                    height: "initial",
+                    background: "none",
+                    opacity: "1",
+                    border: "0",
+                    margin: "0",
+                    padding: "0",
+                    position: "relative",
+                    maxWidth: "100%",
+                  }}
+                >
+                  <img className="block w-auto" src={PlayStore} />
+                </span>
               </a>
+            </div>
           </div>
-          </div>
-          <div className="flex-grow hidden lg:flex md:flex md:justify-items-center lg:justify-start ">
-            <span
-              style={{
-                boxSizing: "border-box",
-                display: "inline-block",
-                overflow: "hidden",
-                width: "initial",
-                height: "initial",
-                background: "none",
-                opacity: "1",
-                border: "0",
-                margin: "0",
-                padding: "0",
-                position: "relative",
-                maxWidth: "100%",
-              }}
-            >
-              <img
-                className="block w-auto"
-                // style={{
-                //   position: "absolute",
-                //   top: "0",
-                //   left: "0",
-                //   bottom: "0",
-                //   right: "0",
-                //   boxSizing: "border-box",
-                //   padding: "0",
-                //   border: "none",
-                //   margin: "auto",
-                //   display: "block",
-                //   width: "0",
-                //   height: "0",
-                //   minwidth: "100%",
-                //   maxWidth: "100%",
-                //   minHeight: "100%",
-                //   maxHeight: "100%",
-                // }}
-                src={AppDownloadRight}
-              />
-            </span>
+          <div className="md:hidden lg:block ">
+            <div className="flex-grow hidden lg:flex md:flex lg:justify-end">
+              <span
+                style={{
+                  boxSizing: "border-box",
+                  display: "inline-block",
+                  overflow: "hidden",
+                  width: "initial",
+                  height: "initial",
+                  background: "none",
+                  opacity: "1",
+                  border: "0",
+                  margin: "0",
+                  padding: "0",
+                  position: "relative",
+                  maxWidth: "100%",
+                }}
+              >
+                <img className="block w-auto" src={AppDownloadRight} />
+              </span>
+            </div>
           </div>
         </div>
       </div>
