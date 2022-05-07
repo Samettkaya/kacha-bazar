e60104eced4373f0e9b5c3b0780bf6110e62a152
samettkaya <kaya67380@gmail.com>
2022-05-07 11:57:33 +0300

    Mobile app added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Navigation from "./components/navigation/Navigation";
 import About from "./pages/About";
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/footer/Footer";
+import MobileApp from "./components/mobileApp/MobileApp";
 function App() {
   return (
     <div>
@@ -14,10 +13,12 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about-us" element={ <About />} />
-       
+        <Route path="about-us" element={<About />} />
       </Routes>
-     
+      <div className="w-full">
+        <MobileApp />
+        <Footer />
+      </div>
     </div>
   );
 }
