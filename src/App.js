36755af83d71fd210f70f660cf5966021d5620de
samettkaya <kaya67380@gmail.com>
2022-05-07 14:36:35 +0300

    code refatoring

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Navigation from "./components/navigation/Navigation";
 import About from "./pages/About";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-<<<<<<< HEAD
+
 import Contact from "./pages/Contact";
-=======
+
 import Footer from "./components/footer/Footer";
 import MobileApp from "./components/mobileApp/MobileApp";
->>>>>>> 64348540cdd8ad13dadb3cd9e48957ce9c024f70
+
 function App() {
   return (
     <div>
@@ -17,12 +17,11 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-        <Route path="about-us" element={ <About />} />
-        <Route path="contact-us" element={ <Contact />} />
-=======
+
         <Route path="about-us" element={<About />} />
->>>>>>> 64348540cdd8ad13dadb3cd9e48957ce9c024f70
+        <Route path="contact-us" element={<Contact />} />
+
+        <Route path="about-us" element={<About />} />
       </Routes>
       <div className="w-full">
         <MobileApp />
